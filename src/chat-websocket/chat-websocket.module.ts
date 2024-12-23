d209782704382ad982264e71ad560a99ae0dcafc
yurii <robka4an@gmail.com>
2024-12-23 14:08:57 +0200

    fix bag with deploy add services in module

--- src/chat-websocket/chat-websocket.module.ts
+++ src/chat-websocket/chat-websocket.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatWebsocketService } from './chat-websocket.service';
 import { ChatWebsocketGateway } from './chat-websocket.gateway';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [ChatWebsocketService, ChatWebsocketGateway],
+  providers: [ChatWebsocketService, ChatWebsocketGateway, PrismaService],
 })
 export class ChatWebsocketModule {}
